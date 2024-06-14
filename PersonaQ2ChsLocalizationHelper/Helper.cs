ad115493fe9fc7d1afec75063fd58fc999b9547e
Xzonn <Xzonn@outlook.com>
2024-06-15 00:15:13 +0800

    Add try-catch

--- PersonaQ2ChsLocalizationHelper/Helper.cs
+++ PersonaQ2ChsLocalizationHelper/Helper.cs
@@ -13,29 +13,33 @@ internal class Helper
       var sheetName = Path.GetRelativePath(inputFolder, filePath).Replace('\\', '/');
       var extension = Path.GetExtension(filePath).ToLowerInvariant();
       IGameData gameData;
-      switch (extension)
+      try
       {
-        case ".arc":
-        case ".bin":
-        case ".pack":
-        case ".tpc":
-          gameData = new BIN(File.ReadAllBytes(filePath));
-          break;
-        case ".pm1":
-          gameData = new PM1(File.ReadAllBytes(filePath));
-          break;
-        case ".bvp":
-          gameData = new BVP("", File.ReadAllBytes(filePath));
-          break;
-        case ".bf":
-          gameData = new BF(File.ReadAllBytes(filePath), "");
-          break;
-        case ".bmd":
-          gameData = new BMD(File.ReadAllBytes(filePath));
-          break;
-        default:
-          continue;
+        switch (extension)
+        {
+          case ".arc":
+          case ".bin":
+          case ".pack":
+          case ".tpc":
+            gameData = new BIN(File.ReadAllBytes(filePath));
+            break;
+          case ".pm1":
+            gameData = new PM1(File.ReadAllBytes(filePath));
+            break;
+          case ".bvp":
+            gameData = new BVP("", File.ReadAllBytes(filePath));
+            break;
+          case ".bf":
+            gameData = new BF(File.ReadAllBytes(filePath), "");
+            break;
+          case ".bmd":
+            gameData = new BMD(File.ReadAllBytes(filePath));
+            break;
+          default:
+            continue;
+        }
       }
+      catch { continue; }
       action(new GameFile(sheetName, gameData), sheetName);
     }
   }
